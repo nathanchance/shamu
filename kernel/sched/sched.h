6fff03690da683a55a480f697360da1f97bf16a6
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2015-02-16 16:42:59 +0530

    sched: Fix bug in average nr_running and nr_iowait calculation
    sched_get_nr_running_avg() returns average nr_running and nr_iowait
    task count since it was last invoked. Fix several bugs in their
    calculation.
    
    * sched_update_nr_prod() needs to consider that nr_running count can
      change by more than 1 when CFS_BANDWIDTH feature is used
    
    * sched_get_nr_running_avg() needs to sum up nr_iowait count across
      all cpus, rather than just one
    
    * sched_get_nr_running_avg() could race with sched_update_nr_prod(),
      as a result of which it could use curr_time which is behind a cpu's
      'last_time' value. That would lead to erroneous calculation of
      average nr_running or nr_iowait.
    
    While at it, fix also a bug in BUG_ON() check in
    sched_update_nr_prod() function and remove unnecessary nr_running
    argument to sched_update_nr_prod() function.
    
    Change-Id: I46737614737292fae0d7204c4648fb9b862f65b2
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    
    Conflicts:
    
    	kernel/sched/fair.c
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1109,7 +1109,7 @@ static inline u64 steal_ticks(u64 steal)
 
 static inline void inc_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
+	sched_update_nr_prod(cpu_of(rq), 1, true);
 	rq->nr_running++;
 
 	if (rq->nr_running >= 2) {
@@ -1132,7 +1132,7 @@ static inline void inc_nr_running(struct rq *rq)
 
 static inline void dec_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
+	sched_update_nr_prod(cpu_of(rq), 1, false);
 	rq->nr_running--;
 }
 
