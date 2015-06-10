912dae0323dc079fdf79ce4762c08b4b641aeba6
arter97 <qkrwngud825@gmail.com>
2015-06-11 00:13:36 +0900

    Introduce Adreno idler for devfreq-based Adreno devices
    Adreno idler by arter97 - Idling algorithm,
    an efficient workaround for msm-adreno-tz's overheads.
    
    Main goal is to lower the power consumptions while maintaining high-performance.
    
    Since msm-adreno-tz tends to *not* use the lowest frequency even on idle,
    Adreno idler replaces msm-adreno-tz's algorithm when it comes to
    calculating idle frequency(mostly by ondemand's method).
    The higher frequencies are not touched with this algorithm, so high-demanding
    games will (most likely) not suffer from worsened performance.
    
    The additional idle_lasttime detects if last 500ms was idle before
    ramping down the frequency to prevent micro-lags on scrolling or playing games.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -20,7 +20,6 @@
 #include <linux/ftrace.h>
 #include <linux/msm_adreno_devfreq.h>
 #include <soc/qcom/scm.h>
-#include <linux/powersuspend.h>
 #include "governor.h"
 
 static DEFINE_SPINLOCK(tz_lock);
@@ -49,9 +48,6 @@ static DEFINE_SPINLOCK(tz_lock);
 #define TZ_INIT_ID		0x6
 
 #define TAG "msm_adreno_tz: "
-
-/* Boolean to detect if pm has entered suspend mode */
-static bool suspended = false;
 
 /* Trap into the TrustZone, and call funcs there. */
 static int __secure_tz_entry2(u32 cmd, u32 val1, u32 val2)
@@ -88,6 +84,11 @@ static void _update_cutoff(struct devfreq_msm_adreno_tz_data *priv,
 	}
 }
 
+#ifdef CONFIG_ADRENO_IDLER
+extern int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
+		 unsigned long *freq);
+#endif
+
 static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 				u32 *flag)
 {
@@ -110,17 +111,21 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		return result;
 	}
 
+	/* Prevent overflow */
+	if (stats.busy_time >= (1 << 24) || stats.total_time >= (1 << 24)) {
+		stats.busy_time >>= 7;
+		stats.total_time >>= 7;
+	}
+
 	*freq = stats.current_frequency;
 	*flag = 0;
 
-	/*
-	 * Force to use & record as min freq when system has
-	 * entered pm-suspend or screen-off state.
-	 */
-	if (suspended || power_suspended) {
-		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
+#ifdef CONFIG_ADRENO_IDLER
+	if (adreno_idler(stats, devfreq, freq)) {
+		/* adreno_idler has asked to bail out now */
 		return 0;
 	}
+#endif
 
 	priv->bin.total_time += stats.total_time;
 	priv->bin.busy_time += stats.busy_time;
@@ -300,8 +305,6 @@ static int tz_resume(struct devfreq *devfreq)
 	struct devfreq_dev_profile *profile = devfreq->profile;
 	unsigned long freq;
 
-	suspended = false;
-
 	freq = profile->initial_freq;
 
 	return profile->target(devfreq->dev.parent, &freq, 0);
@@ -312,8 +315,6 @@ static int tz_suspend(struct devfreq *devfreq)
 	struct devfreq_msm_adreno_tz_data *priv = devfreq->data;
 
 	__secure_tz_entry2(TZ_RESET_ID, 0, 0);
-
-	suspended = true;
 
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
