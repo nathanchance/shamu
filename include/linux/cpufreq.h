3712dac2b2ac8b985089908e0edcafb0fea4561f
brymaster5000 <brymaster5000@gmail.com>
2015-12-26 20:02:27 -0500

    cpufreq: initial commit for smartmax from maxwen

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -454,6 +454,9 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
